d821fff8e13997d1beca28e5a6a038df301d6290
Alexandre Poletto <alexandrepoletto@gmail.com>
2019-03-03 23:21:28 -0800

    created a log function

--- src/index.js
+++ src/index.js
@@ -3,16 +3,18 @@ const chalk = require('chalk');
 const fileHandler = require('./fileHandler');
 const driveService = require('./driveService');
 
+const log = console.log;
+
 async function execute() {
-  console.log(chalk.magenta('Processing File...'));
+  log(chalk.magenta('Processing File...'));
   const testCases = await fileHandler.processFile('input-large.in');
-  console.log(chalk.magenta('File Processed!!!'));
+  log(chalk.magenta('File Processed!!!'));
   const results = [];
   for (testCase of testCases) {
     const result = driveService.findBestPath(testCase);
     const resultString = `Case #${testCase.getNumber()}: ${result}`;
     results.push(resultString);
-    console.log(chalk.green(resultString));
+    log(chalk.green(resultString));
   }
   fileHandler.writeFile(results);
 }
